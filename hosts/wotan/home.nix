800ee8608186a79f39fa5fedf12a94e715d8af9d
Amadeus <amadeus@Amadeuss-MacBook-Pro.local>
2025-11-30 13:58:17 +0100

    feat: add wotan user

---
+++ hosts/wotan/home.nix
@@ -0,0 +1,17 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ../../home/git.nix
+    ../../home/shared.nix
+    ../../home/tmux.nix
+    ../../home/utils.nix
+    ../../home/zsh.nix
+    ../../home/fun.nix
+    ../../home/nixvim/nixvim.nix
+    ../../home/terminals/alacritty.nix
+  ];
+}
